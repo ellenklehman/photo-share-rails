da4d567b9e9a7c4bb8d9e6e0a521888f5476b910
Ellen Lehman <ellenklehman@users.noreply.github.com>
2014-09-02 09:48:04 -0700

    create users and add authentication functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
-  root to: 'photos#index'
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
 
-  resources :users
+  root to: 'users#new'
+
+  resources :users, only: [:new, :create]
   resources :photos
   resources :tags, only: [:new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
 end
